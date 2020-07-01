7efe7a899bc28ba03a66012b02fe0480022189fe
Ryan Carr <ryanfcarr@gmail.com>
2020-07-01 19:06:31 -0400

    Updated Starter App with db functionality and minor visual enhancements

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -26,7 +26,7 @@ class Navigation extends Component {
     return (
       <div className='Navigation'>
         <nav className='navbar navbar-expand-lg navbar-light bg-light mb-3'>
-          <Link className='navbar-brand' to='#'>Project 3</Link>
+          <Link className='navbar-brand' to='#'>Schedule-Me</Link>
           <button className={togglerClass} onClick={this.toggleCollapse} data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
@@ -36,10 +36,6 @@ class Navigation extends Component {
               <li className='nav-item'>
                 <Link className='nav-link' to='/' onClick={this.toggleCollapse}>Home</Link>
               </li>
-              {user &&
-                <li className='nav-item'>
-                  <Link className='nav-link' to='/secret' onClick={this.toggleCollapse}>Secret</Link>
-                </li>}
             </ul>
             <ul className='navbar-nav'>
               {user
--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -1,6 +1,5 @@
 const apiControllers = require('express').Router();
 
 apiControllers.use('/users', require('./usersController'));
-apiControllers.use('/secrets', require('./secretsController'));
 
 module.exports = apiControllers;
