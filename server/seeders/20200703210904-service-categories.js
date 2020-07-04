0e93ffebbbf5fc1edb6ed586b6ff95dcebe7e452
Ryan Carr <ryanfcarr@gmail.com>
2020-07-03 22:23:29 -0400

    Fix ServiceCategory seed

--- server/seeders/20200703210904-service-categories.js
+++ server/seeders/20200703210904-service-categories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('People', [{
+    await queryInterface.bulkInsert('ServiceCategories', [{
       name: "Basic Services",
       isExclusive: true
     },
