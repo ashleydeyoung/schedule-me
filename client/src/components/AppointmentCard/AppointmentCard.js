f0fab550bd30e021ec8d65cb4f06fa174ffae7a3
Ashley DeYoung <ashley.d.deyoung@gmail.com>
2020-07-08 14:15:40 -0400

    added padding to div, created border around card

--- client/src/components/AppointmentCard/AppointmentCard.js
+++ client/src/components/AppointmentCard/AppointmentCard.js
@@ -7,16 +7,16 @@ class AppointmentCard extends Component {
     <div className="card" style={{ padding: "30px" }}>
       {this.props.appointments.map((appointment) => {
         return (
-          <div className="appt-card" style={{ padding: "30px" }}>
+          <div className="card" style={{ padding: "30px", margin: "10px" }}>
             <p>
               <strong>{moment(appointment.startTime).format("LLLL")}</strong>
             </p>
             {appointment.Services.map((Service) => (
-              <p>{Service.name}</p>
+              <p className= "text-primary">{Service.name}</p>
             ))}
             <button
               type="button"
-              className="btn btn-danger content-center"
+              className="btn btn-sm btn-danger content-center mx-4"
               onClick={(e) => this.props.cancelAppointment(appointment.id, e)}>
               Cancel
             </button>
