4fd94ed1889ede781fa133de30fed08770ca669e
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-07-11 20:45:57 -0400

    Add tabs to admin page and ability to change business hours

--- server/controllers/api/index.js
+++ server/controllers/api/index.js
@@ -4,5 +4,6 @@ apiControllers.use('/users', require('./usersController'));
 apiControllers.use('/appointments', require('./appointmentsController'));
 apiControllers.use('/services', require('./servicesController'));
 apiControllers.use('/roles', require('./rolesController'));
+apiControllers.use('/businesssettings', require('./businessSettingsController'));
 
 module.exports = apiControllers;
